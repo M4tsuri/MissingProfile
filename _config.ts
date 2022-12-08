5d0e8967e91e89bab766e3501105d2062a70367c
M4tsuri <me@m4tsuri.com>
2022-12-09 01:03:59 +0800

    feat: added support for latex and code block

--- _config.ts
+++ _config.ts
@@ -1,21 +1,35 @@
 import lume from "lume/mod.ts";
+import metas from "lume/plugins/metas.ts";
+import codeHighlight from "lume/plugins/code_highlight.ts";
+
+import mdKatex from "https://jspm.dev/markdown-it-texmath";
+import mdAttrs from "https://jspm.dev/markdown-it-attrs";
+import mdDeflist from "https://jspm.dev/markdown-it-deflist";
 
 const site = lume({
     location: new URL("https://m4tsuri.com")
+}, {
+    markdown: {
+        plugins: [mdKatex, mdAttrs, mdDeflist],
+        keepDefaultPlugins: true
+    }
 });
 
-// load remote css files
-site.remoteFile("missing-prism.min.css", "https://the.missing.style/v1.0.3/missing-prism.min.css")
-    .remoteFile("missing.min.css", "https://the.missing.style/v1.0.3/missing.min.css");
+site.use(metas())
+site.use(codeHighlight())
+
+// load remote css files, this site is not very famous so we use a offline version here
+site.remoteFile("missing.min.css", "https://the.missing.style/v1.0.3/missing.min.css");
 
 // copy css files
 site.copy("missing.min.css")
-    .copy("missing-prism.min.css")
 
 // copy images
 site.copy("static", ".")
 
 // copy OpenPGP WKD files
 site.copy("static/.well-known", ".well-known");
+
+site.ignore("README.md")
 
 export default site;
---
+++ demo.md
@@ -0,0 +1,56 @@
+---
+layout: layout.njk
+navi:
+    - name: First Link Name
+      url: #
+    - name: Second Link Name
+      url: #
+    - name: Third Link Name
+      url: #
+---
+
+# Title
+
+This is normal markdown.
+
+## Second Level Title
+
+1. list 1
+2. list 2
+3. list 3
+
+> quoted
+>
+> 
+
+| one | two |
+|--|--|
+| 1 | 2 |
+| a table | here | 
+
+
+### Third Level Title
+
+- item 1
+- item 2
+- item 3
+
+#### Forth Level Title
+
+```c
+// code block is supported
+#include <stdio.h>
+
+int main() {
+    printf("hello world");
+    return 0;
+}
+```
+
+inline `code` and $\LaTeX$ are supported
+
+$$
+\sum_{i=0}^{\inf} \texttt{this is also supported}
+$$
+
+
